d33fa701591724ce0dbcdd6ac1c2dc1bb53e162d
manuteja M <mannarapumanuteja@gmail.com>
2024-12-26 08:55:45 +0000

    commit

---
+++ models/staging/order_delete_insert.sql
@@ -0,0 +1,9 @@
+{{ config(materialized="incremental",
+         unique_key='id',
+         incremental_strategy="delete+insert",
+         
+) }}
+
+select * from {{source('datafeed_shared_schema','RAW_ORDER')}} 
+
+
