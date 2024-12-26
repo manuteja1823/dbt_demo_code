6d66538b11885762343ddac7d98e7a0648c1b653
manuteja M <mannarapumanuteja@gmail.com>
2024-12-26 05:16:06 +0000

    commit

--- models/staging/stg_emp.sql
+++ models/staging/stg_emp.sql
@@ -4,6 +4,6 @@ WITH tb1 as(
     select
     id,
     frist_name as NAME,
-    last_name as NAME,
+    last_name as NAME
     from {{source('datafeed_shared_schema','STAGE_EMP')}})
 select * from tb1
